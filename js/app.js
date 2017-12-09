f7a7f8a736487dd4f1ae93d177279bb6fb02e4c7
jordy <jordyhvillavicencio@gmail.com>
2017-12-09 18:37:33 -0500

    console

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 //alert("hola mundo");
-var nombre="jordy villavicencio";
-const comunidad="cabiHack";
-let horario ="taller js";
+var nombre      ="jordy villavicencio";
+const comunidad ="cabiHack";
+let horario     ="taller js";
+//console.log(comunidad);
+//console.log(nombre);
+//console.log(horario);
+console.log("La comunidad "+comunidad+" están dictando el "+horario+" por:"+nombre);
+//console objeto log método
