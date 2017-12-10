bf4fd2e950c2dda23bdbddbf1c755a469fedfb6b
jordy <jordyhvillavicencio@gmail.com>
2017-12-09 19:19:50 -0500

    fin clase

--- js/app.js
+++ js/app.js
@@ -7,3 +7,71 @@ let horario     ="taller js";
 //console.log(horario);
 console.log("La comunidad "+comunidad+" están dictando el "+horario+" por:"+nombre);
 //console objeto log método
+
+//cuántos caracteres tiene la variable comunidad
+console.log("caracteres comunidad")
+console.log(comunidad.length)
+//fin
+
+//ejemplo if
+/*
+var numComunidad=comunidad.length;
+if (numComunidad>8){
+    alert("vamos bien");
+}else{
+    alert("ahi vamos");
+}*/
+
+/*
+var numComunidad=comunidad.length;
+if (numComunidad>10){
+    alert("vamos bien");
+}else if(numComunidad>5){
+    alert("ahi vamos");
+}
+*/
+
+/*
+var notas=15;
+if (notas<10){
+    alert("desaprobado");
+}else if(notas>=10){
+    alert("aprobado");
+}
+*/
+//fin
+
+
+//sumando variables
+/*
+var num1=20;
+var num2=5;
+var resultado;
+
+resultado=num1+num2;
+console.log(resultado);
+resultado=num1*num2;
+console.log(resultado);
+*/
+//fin
+
+
+//boton incompleto
+/*
+var num1=document.getElementById("txt1");
+var num2=document.getElementById("txt2");
+var btn=document.getElementById("btnCalcular");
+
+btnCalcular.addEventListener("click",function(){
+    var resultado=Calcular(num1,num2);
+    console.log(resultado);
+})
+function Calcular(n1,n2){
+    resultado=n1+n2;
+    return resultado;
+}
+*/
+//fin
+
+//https://github.com/GersonDesign
+//gerson perez aguilar facebook
